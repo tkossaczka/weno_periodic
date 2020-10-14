a7679b373e7005dfb4adb666357ce8f6b291a749
tkossaczka <tkossaczka@gmail.com>
2020-10-14 09:39:16 +0200

    exact solution on the fine gird (480 space steps) computed

--- test_tool.py
+++ test_tool.py
@@ -34,6 +34,15 @@ u_ex_5 = torch.load("u_ex_5")
 u_ex_6 = torch.load("u_ex_6")
 u_exs = [u_ex_0, u_ex_1, u_ex_2, u_ex_3, u_ex_4, u_ex_5, u_ex_6]
 
+u_ex_0_fine = torch.load("u_ex_0_fine")
+u_ex_1_fine = torch.load("u_ex_1_fine")
+u_ex_2_fine = torch.load("u_ex_2_fine")
+u_ex_3_fine = torch.load("u_ex_3_fine")
+u_ex_4_fine = torch.load("u_ex_4_fine")
+u_ex_5_fine = torch.load("u_ex_5_fine")
+u_ex_6_fine = torch.load("u_ex_6_fine")
+u_exs_fine = [u_ex_0_fine, u_ex_1_fine, u_ex_2_fine, u_ex_3_fine, u_ex_4_fine, u_ex_5_fine, u_ex_6_fine]
+
 C_vec = np.zeros(rng)
 err_nt_max_vec = np.zeros(rng)
 err_nt_mean_vec = np.zeros(rng)
@@ -58,8 +67,9 @@ for j in range(rng):
     u_t = u_t.detach().numpy()
     _, x, t = my_problem.transformation(u_nt)
     time_steps = t.shape[0]
-    #problem_ex = problem(ic_numb=6, space_steps=60 * 2 * 2, time_steps=None, params=params)
-    u_exact_adjusted = u_exs[j][:,-1]
+    #problem_ex = problem(ic_numb=6, space_steps=60 * 2 * 2 * 2, time_steps=None, params=params)
+    #_, u_exact_adjusted = train_model.compute_exact_end(Buckley_Leverett, problem_ex, 60, time_steps, just_one_time_step=False, trainable=False)
+    u_exact_adjusted = u_exs_fine[j][:,-1]
     error_nt_max = np.max(np.abs(u_nt-u_exact_adjusted.detach().numpy()))
     error_t_max = np.max(np.abs(u_t-u_exact_adjusted.detach().numpy()))
     error_nt_mean = np.mean((u_nt-u_exact_adjusted.detach().numpy())**2)
@@ -82,10 +92,10 @@ err_mat[2,:] = err_nt_mean_vec
 err_mat[3,:] = err_t_mean_vec
 
 
-# params = validation_problems(6)
-# problem_ex_test = problem(ic_numb=6, space_steps=60 * 2 * 2, time_steps=None, params=params)
+# params = validation_problems(0)
+# problem_ex_test = problem(ic_numb=6, space_steps=60 * 2 * 2 * 2, time_steps=None, params=params)
 # _, u_ex_test = train_model.compute_exact(Buckley_Leverett, problem_ex_test, 60, 36, just_one_time_step=False, trainable=False)
-# torch.save(u_ex_test, "u_ex_6")
+# torch.save(u_ex_test, "u_ex_0_fine")
 
 
 
