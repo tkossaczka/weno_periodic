c25b23678ec43da4f2c88e415e1aa65899580589
tkossaczka <tkossaczka@gmail.com>
2021-07-11 13:19:54 +0200

    push before going back to hyperbolic

--- initial_condition_Burgers.py
+++ initial_condition_Burgers.py
@@ -18,15 +18,35 @@ def init_cond_B(ic_numb, x, k = None):
         u0 = k*np.sin(np.pi*x)
     elif ic_numb == 3:  # Riemann problem
         if k == None:
-            k = random.uniform(1, 2)
+            k = 1.5 #random.uniform(1, 2)
         u0 = np.zeros(x.shape[0])
         u0[x >= xmid] = k*1
-    elif ic_numb == 4:
+    elif ic_numb == 4: # asi iba tato funguje
         u0 = 1 +  np.sin(4 * np.pi *x)
     elif ic_numb == 5:
         u0 = 3 +  np.sin(2 * np.pi *x)
     elif ic_numb == 6:
         u0 = 0.5 +  np.sin(6 * np.pi *x)
-
-
+    elif ic_numb == 7:
+        u0 = 2*np.sin(4 * np.pi * x)
+    elif ic_numb == 8:
+        u0 = 1.5*np.cos(np.pi*x)
+    elif ic_numb == 9:
+        u0 = np.sin(2 * np.pi *x)
+    elif ic_numb == 10:
+        u0 = np.sin(3*np.pi *x)
+    elif ic_numb == 11:
+        u0 = 1+ np.sin(3 * np.pi * x)
+    elif ic_numb == 12:
+        u0 = -np.sin((1.2) * np.pi * x)
+    elif ic_numb == 13:
+        u0 = -np.sin((1.5) * np.pi * x)
+    elif ic_numb == 14:
+        u0 = -x*np.sin((1.5) * np.pi * x**2)
+    elif ic_numb == 15:
+        u0 = -x * np.sin((1.5) * np.pi * x)
+    elif ic_numb == 16:
+        u0 = np.sin(np.pi * x ) - (x**3)/2
+    elif ic_numb == 17:
+        u0 = -x * np.sin((1.5) * np.pi * x**3)
     return u0, numb, xmid, height, width, k
--- test_tool_Burgers.py
+++ test_tool_Burgers.py
@@ -16,10 +16,10 @@ problem = Burgers_equation
 train_model = torch.load("C:/Users/Tatiana/Desktop/Research/Research_ML_WENO/Burgers_Equation_Test/Models/Model_78/29.pt")
 
 
-# u_ex_whole = np.load("C:/Users/Tatiana/Desktop/Research/Research_ML_WENO/Burgers_Equation_Test/Test_set_additional/u_exact_IN_4.npy")
-# u_ex = u_ex_whole[0:1024 + 1: 8]
+u_ex_whole = np.load("C:/Users/Tatiana/Desktop/Research/Research_ML_WENO/Burgers_Equation_Test/Test_set_additional/u_exact_IN_15.npy")
+u_ex = u_ex_whole[0:1024 + 1: 8]
 
-space_steps = 1024 #64*2 #*2 # 1024
+space_steps = 128 #64*2 #*2 # 1024
 problem_main = problem(ic_numb=2, space_steps=space_steps, time_steps=None, params=None)
 params = problem_main.get_params()
 ts = problem_main.time_steps
@@ -36,16 +36,119 @@ for k in range(nn):
     u_t = train_model.run_weno(problem_main, u_t, mweno=True, mapped=False, vectorized=True, trainable=True, k=k)
 u_t = u_t.detach().numpy()
 _, x, t = problem_main.transformation(u_nt)
-# error_nt_max = np.max(np.abs(u_nt-u_ex))
-# error_nt_JS_max = np.max(np.abs(u_nt_JS-u_ex))
-# error_t_max = np.max(np.abs(u_t-u_ex))
-# error_nt_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_nt - u_ex) ** 2)))
-# error_nt_JS_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_nt_JS - u_ex) ** 2)))
-# error_t_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_t - u_ex) ** 2)))
+error_nt_max = np.max(np.abs(u_nt-u_ex))
+error_nt_JS_max = np.max(np.abs(u_nt_JS-u_ex))
+error_t_max = np.max(np.abs(u_t-u_ex))
+error_nt_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_nt - u_ex) ** 2)))
+error_nt_JS_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_nt_JS - u_ex) ** 2)))
+error_t_mean = np.sqrt(2 / space_steps) * (np.sqrt(np.sum((u_t - u_ex) ** 2)))
 plt.plot(x, u_nt, color='blue', marker='o')
 plt.plot(x, u_nt_JS, color='green', marker='o')
 plt.plot(x, u_t, marker='o', color='red')
-# plt.plot(x, u_ex, color='black')
+#plt.plot(x, u_ex, color='black')
 
-# np.save("C:/Users/Tatiana/Desktop/Research/Research_ML_WENO/Burgers_Equation_Test/Test_set_additional/u_exact_IN_2.npy", u_nt)
+# np.save("C:/Users/Tatiana/Desktop/Research/Research_ML_WENO/Burgers_Equation_Test/Test_set_additional/u_exact_IN_17.npy", u_nt)
 
+
+# # IN_15
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=1)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_15.pdf", bbox_inches='tight')
+
+# # IN_14
+# h = problem_main.h
+# x_ex = np.linspace(-1, 1 - h, 1024)
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_14.pdf", bbox_inches='tight')
+
+# # IN_13
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=2)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_13.pdf", bbox_inches='tight')
+
+# # IN_12
+# h = problem_main.h
+# x_ex = np.linspace(0, 2 - h, 1024)
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x_ex, u_ex_whole, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_12.pdf", bbox_inches='tight')
+
+# # IN_2
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=1)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_2.pdf", bbox_inches='tight')
+
+# # IN_3
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_3.pdf", bbox_inches='tight')
+
+# # IN_10
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_10.pdf", bbox_inches='tight')
+
+# # IN_4
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_4.pdf", bbox_inches='tight')
+
+# # IN_7
+# fig, ax = plt.subplots()
+# ax.plot(x, u_nt_JS, color='blue') #, marker='o')
+# ax.plot(x, u_nt, color='green') #, marker='o')
+# ax.plot(x, u_t, color='red', marker='x')
+# ax.plot(x, u_ex, color='black')
+# ax.legend(('WENO-JS', 'WENO-Z', 'WENO-DS', 'ref. sol.'), loc=4)
+# ax.set_xlabel('x')
+# ax.set_ylabel('u')
+# plt.savefig("BE_IN_7.pdf", bbox_inches='tight')
